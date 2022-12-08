ba00ac03211c6b4392cd705c5c417e06cd85d420
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2022-12-08 15:44:33 +0000

    done

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,4 +1,5 @@
-_dictionary):
+#!/usr/bin/python3
+def number_keys(a_dictionary):
     num = 0
     list_keys = list(a_dictionary.keys())
 
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
-orted_dictionary(a_dictionary):
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
     list_ord = list(a_dictionary.keys())
     list_ord.sort()
     for i in list_ord:
